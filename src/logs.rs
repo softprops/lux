8a5bf6b1f0391eb04edde32ce56bef53970ec20e
softprops <d.tangren@gmail.com>
2017-01-16 12:18:30 -0500

    docs and formatting

--- src/logs.rs
+++ src/logs.rs
@@ -10,6 +10,7 @@ use super::url;
 
 const PROXY_HOST: &'static str = "http://127.0.0.1:8001";
 
+/// A single log line
 #[derive(Debug)]
 struct Record {
     namespace: String,
@@ -19,15 +20,24 @@ struct Record {
     text: String,
 }
 
+/// Log request options
 #[derive(Default, Debug)]
 pub struct Logs {
+    /// Follow the log stream of the pod(s)
     follow: bool,
+    /// A selector to restrict the list of returned objects by their [label specification](https://kubernetes.io/docs/user-guide/labels/)
     label: Option<String>,
+    /// Optional cluster [namespace](https://kubernetes.io/docs/user-guide/namespaces/)
     namespace: Option<String>,
+    /// Return previous terminated container logs
     previous: bool,
+    /// Fetch logs since this many seconds ago
     since: Option<i32>,
+    /// Fetch logs since this RFC3339 timestamp
     since_time: Option<String>,
+    /// Tail only this many log lines
     tail: Option<i32>,
+    /// Include log timestamps
     timestamps: bool,
 }
 
@@ -53,6 +63,7 @@ impl Logs {
         }
     }
 
+    /// start fetching stream of logs
     pub fn fetch(&self) -> Result<(), Error> {
         let mut colors = color::Wheel::new();
         let (tx, rx) = channel();
--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,14 @@ fn main() {
     let args = App::new("lux")
         .about("a kubernetes log multiplexor")
         .args_from_usage("-l, --label=[LABEL] 'Limits record to those that match a selector filter'
-             -f, --follow \
-                          'Follows the logs as they are available'
-             -n, \
-                          --namespace=[NAMESPACE] 'Limits records to those from pods under a target namespace'
-             \
-                          -t, --timestamps 'Prints record timestamps'
-             --tail=[N] \
-                          'Limits number of recent log records to display'
-             --since=[SECONDS] \
-                          'Prints records since this a given number of seconds. Only one of \
-                          since-time / since may be used.'
-              \
-                          --since-time=[RFC3339_TIMESTAMP] 'Prints records since the given \
+             -p, --previous 'Return previous terminated container logs.'
+             -f, --follow 'Follows the logs as they are available'
+             -n, --namespace=[NAMESPACE] 'Limits records to those from pods under a target namespace'
+             -t,--timestamps 'Prints record timestamps'
+             --tail=[N] 'Limits number of recent log records to display'
+             --since=[SECONDS] 'Prints records since this a given number of \
+                          seconds. Only one of since-time / since may be used.'
+             --since-time=[RFC3339_TIMESTAMP] 'Prints records since the given \
                           timestamp Only one of since-time / since may be used.'
         ")
         .get_matches();
